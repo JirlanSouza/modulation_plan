433145859c938d12259135c33f0fe831140c2a2d
Jirlan Souza <jirlansouza08@gmail.com>
2023-11-30 20:45:54 -0400

    feat: add the save button on the plan modulation page

--- src/app/plan-modulation/PlanModulationTable.tsx
+++ src/app/plan-modulation/PlanModulationTable.tsx
@@ -26,82 +26,85 @@ export function PlanModulationTable({
                     <span className="ml-2 md:ml-4 text-xs"> {date}</span>
                 </div>
 
-                <table
-                    className="
-                    w-full
+                <div className="rounded-xl overflow-hidden max-w-fit">
+                    <table
+                        className="
+                    w-fit
+                    max-w-sm
                     rounded-xl
+                    [&>*]:border [&>*]: border-slate-400
                     text-xs md:text-sm
-                    border-collapse border border-slate-500
-                    overflow-hidden"
-                >
-                    <thead>
-                        <tr>
-                            <th className="bg-slate-200 border border-slate-600">
-                                Area
-                            </th>
-                            <th className="bg-slate-200 border border-slate-600">
-                                Sub area
-                            </th>
-                            {planModulationData[0].subAreas[0].modulation.d0.hours.map(
-                                (hour) => (
-                                    <th
-                                        key={hour.hour}
-                                        className="bg-slate-200 border border-slate-600 px-0.5"
-                                    >
-                                        {hour.hour}
-                                    </th>
-                                )
-                            )}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {planModulationData.map((area, areaIndex) =>
-                            area.subAreas.map((subArea, subAreaIndex) => (
-                                <tr
-                                    key={subArea.name}
-                                    data-selectionArea={hasSelectionArea}
-                                    className="
-                                    bg-slate-200
-                                    border-y-2 border-slate-900
-                                    data-[selectionArea=true]:hover:border-y-yellow-200"
-                                >
-                                    <td className="border border-y-2 border-slate-600">
-                                        {area.name}
-                                    </td>
-                                    <td className="border border-y-2 border-slate-600">
-                                        {subArea.name}
-                                    </td>
-
-                                    {subArea.modulation[
-                                        isDZero ? "d0" : "dPlus1"
-                                    ].hours.map((hour, hourIndex) => (
-                                        <td
-                                            key={subArea.name + hour.hour}
-                                            data-state={hour.state}
-                                            className="
-                                            w-8 h-8
-                                            bg-red-600 data-[state=true]:bg-green-600
-                                            border-x border-slate-600                                           
-                                            hover:brightness-90 cursor-pointer"
-                                            onClick={() =>
-                                                toggleHour({
-                                                    areaIndex,
-                                                    subAreaIndex,
-                                                    day: isDZero
-                                                        ? "d0"
-                                                        : "dPlus1",
-                                                    hourIndex,
-                                                })
-                                            }
+                    whitespace-nowrap                    
+                    overflow-auto"
+                    >
+                        <thead>
+                            <tr className="[&>*]:border-x [&>*]:border-slate-400 [&>first]:border-0">
+                                <th className="w-max px-2 py-1 bg-slate-200 ">
+                                    Area
+                                </th>
+                                <th className="px-2 bg-slate-200 ">Sub area</th>
+                                {planModulationData[0].subAreas[0].modulation.d0.hours.map(
+                                    (hour) => (
+                                        <th
+                                            key={hour.hour}
+                                            className="bg-slate-200 px-1"
                                         >
-                                            {hour.coment}
+                                            {hour.hour}
+                                        </th>
+                                    )
+                                )}
+                            </tr>
+                        </thead>
+                        <tbody className="overflow-auto">
+                            {planModulationData.map((area, areaIndex) =>
+                                area.subAreas.map((subArea, subAreaIndex) => (
+                                    <tr
+                                        key={subArea.name}
+                                        data-selectionArea={hasSelectionArea}
+                                        className="
+                                    bg-slate-200
+                                    
+                                    data-[selectionArea=true]:hover:border-y-yellow-200"
+                                    >
+                                        <td className="px-2  border-slate-600">
+                                            {area.name}
                                         </td>
-                                    ))}
-                                </tr>
-                            ))
-                        )}
-                    </tbody>
-                </table>
+                                        <td className="px-2  border-slate-600">
+                                            {subArea.name}
+                                        </td>
+
+                                        {subArea.modulation[
+                                            isDZero ? "d0" : "dPlus1"
+                                        ].hours.map((hour, hourIndex) => (
+                                            <td
+                                                key={subArea.name + hour.hour}
+                                                data-state={hour.state}
+                                                className="
+                                            w-4 sm:w-8
+                                            h-4 sm:h-8
+                                            bg-red-600 data-[state=true]:bg-green-600
+                                             border-slate-600                                           
+                                            hover:brightness-90 cursor-pointer"
+                                                onClick={() =>
+                                                    toggleHour({
+                                                        areaIndex,
+                                                        subAreaIndex,
+                                                        day: isDZero
+                                                            ? "d0"
+                                                            : "dPlus1",
+                                                        hourIndex,
+                                                    })
+                                                }
+                                            >
+                                                {hour.coment}
+                                            </td>
+                                        ))}
+                                    </tr>
+                                ))
+                            )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     );
--- src/app/plan-modulation/page.tsx
+++ src/app/plan-modulation/page.tsx
@@ -18,28 +18,46 @@ export default function PlanModulation() {
             <div className="flex justify-between">
                 <h1 className="text-md md:text-lg">Planejar modulação</h1>
 
-                <span className="flex flex-col md:gap-y-1 text-slate-700">
-                    <label className="text-xs">Selecione a data</label>
-                    <input
-                        type="date"
+                <span className="flex gap-2 items-end">
+                    <button
                         className="
-                            h-6 md:h8
+                    h-6 md:h-8
+                    mr-2
+                    w-28 max-w-full
+                    justify-self-end
+                    bg-green-500 hover:bg-green-600
+                    rounded-md md:rounded-lg
+                    px-4
+                    text-slate-800 hover:text-slate-200
+                    active:scale-x-95
+                    transition-all"
+                    >
+                        Salvar
+                    </button>
+
+                    <span className="flex flex-col md:gap-y-1 text-slate-700">
+                        <label className="text-xs">Selecione a data</label>
+                        <input
+                            type="date"
+                            className="
+                            h-6 md:h-8
                             text-sm md:text-base
                             rounded-md md:rounded-lg
-                            p-1.5 md:p2
+                            px-2 md:p-4
                             bg-slate-200
                             outline-cyan-500
                             select-none"
-                        value={selectedDate.toISOString().slice(0, 10)}
-                        onChange={(e) =>
-                            setSelectedDate(new Date(e.target.value))
-                        }
-                        onKeyDown={(e) => {
-                            e.preventDefault();
-                            e.stopPropagation();
-                        }}
-                        onFocus={(e) => e.target.showPicker()}
-                    />
+                            value={selectedDate.toISOString().slice(0, 10)}
+                            onChange={(e) =>
+                                setSelectedDate(new Date(e.target.value))
+                            }
+                            onKeyDown={(e) => {
+                                e.preventDefault();
+                                e.stopPropagation();
+                            }}
+                            onFocus={(e) => e.target.showPicker()}
+                        />
+                    </span>
                 </span>
             </div>
 
