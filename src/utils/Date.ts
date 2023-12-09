56100979df5b13f3c33a649a6ad629d5b3bebe3a
Jirlan Souza <jirlansouza08@gmail.com>
2023-12-09 15:19:46 -0400

    feat: cereated the `dateFromInputToDate` function

--- src/utils/Date.ts
+++ src/utils/Date.ts
@@ -3,3 +3,12 @@ export function addDays(date: Date, days: number) {
     newDate.setDate(date.getDate() + days);
     return newDate;
 }
+
+export function dateFromInputToDate(date: string, timeZoneOffset: number) {
+    const absTimeZoneOffsetString = Math.abs(timeZoneOffset).toString();
+    const dateStr = `${date}T00:00:00.000${timeZoneOffset < 0 ? "-" : "+"}${
+        absTimeZoneOffsetString.length > 1 ? "" : "0"
+    }${absTimeZoneOffsetString}:00`;
+
+    return new Date(dateStr);
+}
